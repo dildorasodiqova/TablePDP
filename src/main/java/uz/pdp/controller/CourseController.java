4a913c961bbf53a6c066f41b508cb4ee8f6839cb
Madina <eshpulatovadina@gmail.com>
2023-10-08 11:33:56 +0500

    user Auth setted

--- src/main/java/uz/pdp/controller/CourseController.java
+++ src/main/java/uz/pdp/controller/CourseController.java
@@ -1,7 +1,19 @@
 package uz.pdp.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import uz.pdp.DTO.requestDTO.CourseCreateDTO;
+import uz.pdp.DTO.responceDTO.CourseResponseDTO;
+import uz.pdp.service.courseService.CourseServiceImpl;
 
 @RestController
+@RequiredArgsConstructor
 public class CourseController {
+    private final CourseServiceImpl courseService;
+    @PostMapping
+    public CourseResponseDTO create(@RequestBody CourseCreateDTO dto ) {
+        return courseService.create(dto);
+    }
 }
